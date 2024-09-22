0e2ba0b3cf36acad777b9f419d90d5c62e982547
Rohil Bansal <94187115+realrohilbansal@users.noreply.github.com>
2024-09-22 19:08:38 +0530

    vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 # build_files.sh
+#!/bin/bash
+echo "Python version:"
+python --version
+echo "Pip version:"
+pip --version
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
