ea0fba24211c737d1ddd516d96d6bdbde599884f
Rohil Bansal <94187115+realrohilbansal@users.noreply.github.com>
2024-09-22 19:15:09 +0530

    vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,15 @@
 # build_files.sh
 #!/bin/bash
 echo "Building the project..."
-python3 -m pip install --upgrade pip
-pip3 install -r requirements.txt
+python -m pip install --upgrade pip
+pip install -r requirements.txt
 
 if [ -n "$DATABASE_URL" ]; then
     echo "Performing database migrations..."
-    python3 manage.py migrate --noinput
+    python manage.py migrate --noinput
 else
     echo "No DATABASE_URL found. Skipping migrations."
 fi
 
 echo "Collect static..."
-python3 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
--- studybud/settings.py
+++ studybud/settings.py
@@ -88,14 +88,21 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600,
-        conn_health_checks=True,
-        ssl_require=True,
-    )
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
