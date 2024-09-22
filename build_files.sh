f2d00e2c24b83ca0bc4ee695605db56d226b3ed8
Rohil Bansal <94187115+realrohilbansal@users.noreply.github.com>
2024-09-22 19:10:37 +0530

    vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,12 @@
 # build_files.sh
 #!/bin/bash
 echo "Building the project..."
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
 
 echo "Make migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collect static..."
-python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
